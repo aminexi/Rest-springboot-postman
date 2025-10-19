070df2b4de7d075ebe48f2d3acf1a41ea3e62fba
Amine ELFALKI <amine.elfalki@gmail.com>
2025-10-19 21:00:32 +0100

    move project to root

---
+++ src/main/java/com/spring/demo/service/IService.java
@@ -0,0 +1,13 @@
+package com.spring.demo.service;
+
+import com.spring.demo.domaine.ArticleDTO;
+
+import java.util.List;
+public interface IService {
+    ArticleDTO getById(Long id);
+    List<ArticleDTO> getAll();
+    void create(ArticleDTO article);
+    void update(Long id, ArticleDTO article);
+    void deleteById(Long id);
+}
+
---
+++ src/main/java/com/spring/demo/service/model/Article.java
@@ -0,0 +1,16 @@
+package com.spring.demo.service.model;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Article implements Serializable {
+    private Long id;
+    private String description;
+    private Double price;
+    private Double quantity;
+}
